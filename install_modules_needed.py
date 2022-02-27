c67864f2f93b79cc7d68c0c24c418a5c249a220d
jayendrabharti <70205687+jaybharti2804@users.noreply.github.com>
2022-02-27 12:07:55 +0530

    Update install_modules_needed.py

--- install_modules_needed.py
+++ install_modules_needed.py
@@ -5,6 +5,17 @@ def import_or_install(package):
     except ImportError:
         print("Insalling",package)
         pip.main(['install', package])   
-list_of_modules = ['mysql.connector','shutil','tabulate', 'pwinput' ,'time','os','pafy','vlc','tkinter','datetime','subprocess','youtube_search']
+list_of_modules = ['mysql.connector',
+                   'shutil',
+                   'tabulate', 
+                   'pwinput',
+                   'time',
+                   'os',
+                   'pafy',
+                   'vlc',
+                   'tkinter',
+                   'datetime',
+                   'subprocess',
+                   'youtube_search']
 for package in list_of_modules:
     import_or_install(package)
